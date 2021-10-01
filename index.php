ff75bec9a24ab8d1bf70e980101eed4924a0009f
Puji Rahmawati <pujirahma9876@gmail.com>
2021-10-01 13:02:23 +0700

    menambahkan lsit

--- index.php
+++ index.php
@@ -11,6 +11,8 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, nam ea dolorem, culpa blanditiis corporis molestiae ipsum modi debitis officia labore aliquid atque magni expedita amet veniam ex odio alias recusandae quam provident tempore consequuntur at perspiciatis. Dolore rerum doloribus, quibusdam architecto, sapiente iste maxime optio porro exercitationem necessitatibus repellat vitae ratione culpa inventore praesentium perferendis quo reiciendis amet. Quis, blanditiis ratione sunt assumenda eos ad minus natus amet deleniti, at totam voluptate. Nesciunt qui hic dolorem minus facilis est. Itaque deserunt quo quos aut porro numquam atque quisquam autem fugiat nisi molestiae corporis doloribus repellat aspernatur magnam voluptate velit, qui vel non voluptas unde eligendi ex? Possimus, earum blanditiis voluptatem neque at nulla error eaque fugit, porro ad libero alias voluptatibus repudiandae illo repellendus aliquam adipisci laborum nisi necessitatibus provident praesentium tempora dolor commodi et! Velit esse repudiandae sunt quos, cum quisquam vel ipsa necessitatibus soluta ut rem, voluptatum accusamus qui quibusdam. Excepturi accusamus eos enim deleniti impedit, eligendi odit soluta non voluptates quibusdam sequi laboriosam porro. Sint doloribus possimus quo repudiandae fuga, rem inventore beatae vitae, expedita perspiciatis ab voluptatum nemo est accusantium nisi ex blanditiis eaque voluptatem magnam provident cum. Deleniti, commodi. Dolor hic quisquam quam repellendus.</p>
+    <li>satu</li>
+    <li>dua</li>
 </body>
 
 </html>
